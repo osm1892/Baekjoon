1a2da221d7b364605c43664086e7e61c42fd4317
osm1892 <osm1892@sungkyul.ac.kr>
2022-06-26 21:50:00 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Solution();
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    Solution();
+}
+
+void Solution()
+{
+    string heung;
+    cin >> heung;
+    cin.ignore();
+    
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << heung << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
